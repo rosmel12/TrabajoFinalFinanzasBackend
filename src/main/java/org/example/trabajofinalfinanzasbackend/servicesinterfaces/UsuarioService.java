8adbfac1a099b577b26dc34825f019482e2790e3
Rosmel Gomez Benancio <benancio.del1@gmail.com>
2024-10-27 11:20:07 -0500

    gaaa

--- src/main/java/org/example/trabajofinalfinanzasbackend/servicesinterfaces/UsuarioService.java
+++ src/main/java/org/example/trabajofinalfinanzasbackend/servicesinterfaces/UsuarioService.java
@@ -3,6 +3,8 @@ package org.example.trabajofinalfinanzasbackend.servicesinterfaces;
 import org.example.trabajofinalfinanzasbackend.model.Usuario;
 import org.example.trabajofinalfinanzasbackend.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -15,7 +17,9 @@ public class UsuarioService {
         Usuario usuarioComprobar=usuarioRepository.findUsuarioByUsername(usuario.getUsername());
         if(usuarioComprobar==null){
             String pass=usuario.getPassword();
-            usuario.setPassword(pass);
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String encodedPassword = passwordEncoder.encode(pass);
+            usuario.setPassword(encodedPassword);
            usuario=usuarioRepository.save(usuario);
            return usuario.getId();
         }
