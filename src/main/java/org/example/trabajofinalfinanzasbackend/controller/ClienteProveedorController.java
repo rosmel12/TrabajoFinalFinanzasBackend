700fbc0ddbdcbff4b9d871fea5efbda4afee04c2
Rosmel Gomez Benancio <benancio.del1@gmail.com>
2024-11-01 14:24:28 -0500

    gaaa

--- src/main/java/org/example/trabajofinalfinanzasbackend/controller/ClienteProveedorController.java
+++ src/main/java/org/example/trabajofinalfinanzasbackend/controller/ClienteProveedorController.java
@@ -7,9 +7,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 @RequestMapping("/quma/clienteproveedor")
 @CrossOrigin()
